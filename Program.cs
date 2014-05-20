c403bcf13552fd183adcab401f78dcd279ca8adc
Emma <emmanuellecoel@yahoo.fr>
2014-05-20 19:44:41 +0200

    Ajout du projet

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Sudoku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //todo: Voir pour récupérer en entrée un sudoku, on en discutera ensemble mais sinon je pensais à un string que l'on split ensuite
+            //todo: Par exemple: 0_6_0_2_0_0_0_0_5
+            var input = new[]
+            {
+                new[] { 0, 6, 0, 2, 0, 0, 0, 0, 5 },
+                new[] { 0, 0, 0, 9, 0, 0, 0, 0, 3 },
+                new[] { 9, 0, 2, 0, 6, 0, 8, 4, 0 },
+                new[] { 0, 0, 4, 0, 0, 9, 0, 5, 6 },
+                new[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                new[] { 6, 8, 0, 5, 0, 0, 1, 0, 0 },
+                new[] { 0, 2, 7, 0, 9, 0, 6, 0, 4 },
+                new[] { 1, 0, 0, 0, 0, 2, 0, 0, 0 },
+                new[] { 4, 0, 0, 0, 0, 1, 0, 8, 0 }
+            };
+
+            //Instanciate Sudoku
+            var sudoku = new Sudoku(input);
+
+            //Resolve Sudoku
+            sudoku.isValid(0);
+
+            //Print result
+            Console.WriteLine("Sudoku Solution:");
+            sudoku.PrintGrid();
+
+            //Stop
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sudoku.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace Sudoku
+{
+    public class Sudoku
+    {
+        #region Initialization
+
+        public Sudoku(int[][] grid)
+        {
+            Grid = grid;
+        }
+
+        private readonly int[][] Grid;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// This method is designed to print the grid
+        /// is missing from the given line.
+        /// </summary>
+        public void PrintGrid()
+        {
+            Console.WriteLine(" -----------------");
+
+            for (var line = 0; line < 9; line++)
+            {
+                for (var row = 0; row < 9; row++)
+                {
+                    Console.Write("|{0}{1}", Grid[line][row], row == 8 ? "|" : "");
+                }
+
+                Console.Write("\n");
+                if ((line + 1) % 3 == 0)
+                    Console.WriteLine(" -----------------");
+            }
+
+            Console.WriteLine("\n\n");
+        }
+
+        /// <summary>
+        /// This method is designed to check if the given value
+        /// is missing from the given line.
+        /// </summary>
+        /// <param name="value"> Value to check</param>
+        /// <param name="line"> Line to iterate</param>
+        public bool isMissingOnLine(int value, int line)
+        {
+            for (var row = 0; row < 9; row++)
+            {
+                if (Grid[line][row] == value)
+                    return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// This method is designed to check if the given value
+        /// is missing from the given row.
+        /// </summary>
+        /// <param name="value"> Value to check</param>
+        /// <param name="row"> Row to iterate</param>
+        public bool isMissingOnColumn(int value, int row)
+        {
+            for (var line = 0; line < 9; line++)
+            {
+                if (Grid[line][row] == value)
+                    return false;
+            }
+
+            return true;   
+        }
+
+        /// <summary>
+        /// This method is designed to check if the given value
+        /// is missing from the given Block.
+        /// </summary>
+        /// <param name="value"> Value to check</param>
+        /// <param name="line"> Line to iterate</param>
+        /// <param name="row"> Row to iterate</param>
+        public bool isMissingOnBlock(int value, int line, int row)
+        {
+            int _line = line - (line % 3), _row = row - (row % 3);
+            for (line = _line; line < _line + 3; line++)
+            {
+                for (row = _row; row < _row + 3; row++)
+                {
+                    if (Grid[line][row] == value)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// This method check is the Grid appear to be valid.
+        /// </summary>
+        /// <param name="position"> Check position</param>
+        public bool isValid(int position)
+        {
+            while (true)
+            {
+                if (position == (9 * 9))
+                    return true;
+
+                int line = (position / 9), row = (position % 9);
+
+                if (Grid[line][row] != 0)
+                {
+                    position = (position + 1);
+                    continue;
+                }
+
+                for (var value = 1; value <= 9; value++)
+                {
+                    if (!isMissingOnLine(value, line) || !isMissingOnColumn(value, row) || !isMissingOnBlock(value, line, row))
+                        continue;
+                    Grid[line][row] = value;
+
+                    if (isValid(position + 1))
+                        return true;
+                }
+
+                Grid[line][row] = 0;
+
+                return false;
+            }
+        }
+
+        #endregion
+    }
+}
