423ba72a692629ecf2e14be7146c89bc04638191
Emma <emmanuellecoel@yahoo.fr>
2014-06-02 21:57:26 +0200

    Changements ...

--- Program.cs
+++ Program.cs
@@ -9,79 +9,72 @@ namespace Sudoku
 
             //facile
             
-                        var input = new[]
-                        {
-                            new[]{6,0,0, 0,0,0, 4,0,8},
-                            new[]{0,4,7, 0,5,8, 3,0,2},
-                            new[]{9,3,0, 7,0,2, 0,0,0},
+            var grille_facile = new[]
+            {
+                new[]{6,0,0, 0,0,0, 4,0,8},
+                new[]{0,4,7, 0,5,8, 3,0,2},
+                new[]{9,3,0, 7,0,2, 0,0,0},
 
-                            new[]{0,9,0, 0,0,1, 7,0,4},
-                            new[]{0,7,0, 0,0,0, 0,2,0},
-                            new[]{4,0,2, 9,0,0, 0,8,0},
+                new[]{0,9,0, 0,0,1, 7,0,4},
+                new[]{0,7,0, 0,0,0, 0,2,0},
+                new[]{4,0,2, 9,0,0, 0,8,0},
 
-                            new[]{0,0,0, 1,0,7, 0,3,5},
-                            new[]{7,0,3, 4,9,0, 2,1,0},
-                            new[]{5,0,9, 0,0,0, 0,0,7}
-                         };
+                new[]{0,0,0, 1,0,7, 0,3,5},
+                new[]{7,0,3, 4,9,0, 2,1,0},
+                new[]{5,0,9, 0,0,0, 0,0,7}
+            };
             
 
-            //facile
-            /*
-                        var input = new[]
-                        {
-                            new[]{0,6,0, 0,0,0, 0,0,2},
-                            new[]{3,9,0, 1,2,0, 7,8,6},
-                            new[]{0,0,7, 8,0,0, 0,0,0},
+            //facile - 2
+            var grille_facile2 = new[]
+            {
+                new[]{0,6,0, 0,0,0, 0,0,2},
+                new[]{3,9,0, 1,2,0, 7,8,6},
+                new[]{0,0,7, 8,0,0, 0,0,0},
 
-                            new[]{8,3,0, 0,0,2, 5,0,9},
-                            new[]{1,0,0, 6,0,3, 0,0,8},
-                            new[]{7,0,9, 5,0,0, 0,6,4},
+                new[]{8,3,0, 0,0,2, 5,0,9},
+                new[]{1,0,0, 6,0,3, 0,0,8},
+                new[]{7,0,9, 5,0,0, 0,6,4},
 
-                            new[]{0,0,0, 0,0,5, 4,0,0},
-                            new[]{6,7,2, 0,4,1, 0,9,5},
-                            new[]{5,0,0, 0,0,0, 0,3,0}
-                         };
-            */
-
-            //démoniaque
-            /*
-                        var input = new[]
-                        {
-                            new[]{0,1,2, 4,5,0, 3,0,0},
-                            new[]{0,0,0, 2,7,0, 0,0,4},
-                            new[]{4,0,0, 1,0,0, 0,0,7},
-
-                            new[]{0,0,0, 0,0,0, 5,9,3},
-                            new[]{2,7,0, 0,0,0, 0,8,6},
-                            new[]{9,3,6, 0,0,0, 0,0,0},
-
-                            new[]{3,0,0, 0,0,2, 0,0,5},
-                            new[]{5,0,0, 0,6,1, 0,0,0},
-                            new[]{0,0,7, 0,9,5, 8,4,0}
-                         };
-            */
+                new[]{0,0,0, 0,0,5, 4,0,0},
+                new[]{6,7,2, 0,4,1, 0,9,5},
+                new[]{5,0,0, 0,0,0, 0,3,0}
+            };
 
             //moyen
-            /*
-                        var input = new[]
-                        {
-                            new[]{3,0,0, 0,0,0, 0,2,0},
-                            new[]{5,7,0, 0,4,0, 6,0,9},
-                            new[]{1,0,0, 0,2,9, 5,0,0},
+            var grille_moyen = new[]
+            {
+                new[]{3,0,0, 0,0,0, 0,2,0},
+                new[]{5,7,0, 0,4,0, 6,0,9},
+                new[]{1,0,0, 0,2,9, 5,0,0},
 
-                            new[]{0,0,0, 3,6,4, 0,8,0},
-                            new[]{0,0,0, 0,0,0, 0,0,0},
-                            new[]{0,3,0, 2,9,5, 0,0,0},
+                new[]{0,0,0, 3,6,4, 0,8,0},
+                new[]{0,0,0, 0,0,0, 0,0,0},
+                new[]{0,3,0, 2,9,5, 0,0,0},
 
-                            new[]{0,0,3, 4,5,0, 0,0,6},
-                            new[]{9,0,4, 0,8,0, 0,7,3},
-                            new[]{0,8,0, 0,0,0, 0,0,1}
-                         };
-            */
+                new[]{0,0,3, 4,5,0, 0,0,6},
+                new[]{9,0,4, 0,8,0, 0,7,3},
+                new[]{0,8,0, 0,0,0, 0,0,1}
+            };
+
+            //démoniaque
+            var grille_demoniaque = new[]
+            {
+                new[]{0,1,2, 4,5,0, 3,0,0},
+                new[]{0,0,0, 2,7,0, 0,0,4},
+                new[]{4,0,0, 1,0,0, 0,0,7},
+
+                new[]{0,0,0, 0,0,0, 5,9,3},
+                new[]{2,7,0, 0,0,0, 0,8,6},
+                new[]{9,3,6, 0,0,0, 0,0,0},
+
+                new[]{3,0,0, 0,0,2, 0,0,5},
+                new[]{5,0,0, 0,6,1, 0,0,0},
+                new[]{0,0,7, 0,9,5, 8,4,0}
+            };
 
             //difficile - irréalisable version humain
-            /*
-            var input = new[]
+            var grille_difficile = new[]
             {
                 new[]{0,0,0, 0,2,3, 8,0,0},
                 new[]{0,0,0, 0,0,0, 0,0,0},
@@ -94,56 +87,95 @@ namespace Sudoku
                 new[]{3,1,0, 0,0,0, 0,4,9},
                 new[]{0,0,0, 0,0,0, 0,0,0},
                 new[]{0,0,2, 8,5,0, 0,0,0}
-             };*/
+            };
 
-            Console.WriteLine("Quelle solution voulez-vous?");
-            Console.WriteLine("1. Par solution humaine");
-            Console.WriteLine("2. Par solution machine");
-            String solution_choisie = Console.ReadLine();
+            Console.WriteLine("********************************************");
+            Console.WriteLine("************** SUDOKU SOLVER ***************");
+            Console.WriteLine("********************************************");
 
-            if (solution_choisie == "1") 
+            String solution_choisie = "";
+           
+            do
             {
-                // On définit un objet de type "Sudoku"
-                var sudokuHumain = new SudokuHumain(input);
-                sudokuHumain.afficherSudoku();
+                Console.WriteLine("Quelle force de Sudoku voulez-vous?");
+                Console.WriteLine("Taper 1 - Facile");
+                Console.WriteLine("Taper 2 - Moyen");
+                Console.WriteLine("Taper 3 - Difficile");
+                Console.WriteLine("Taper 4 - Démoniaque");
+                String force_choisie = Console.ReadLine();
 
-                DateTime start = DateTime.Now;
+                int[][] grille_finale = new int[9][];
 
-                // On démarre la résolution en commencant par 0
-                sudokuHumain.resolution();
+                switch (force_choisie)
+                {
+                    case "1":
+                        grille_finale = grille_facile;
+                        break;
+                    case "2":
+                        grille_finale = grille_moyen;
+                        break;
+                    case "3":
+                        grille_finale = grille_difficile;
+                        break;
+                    case "4":
+                        grille_finale = grille_demoniaque;
+                        break;
+                    default:
+                        grille_finale = grille_facile2;
+                        break;
+                }
 
-                DateTime end = DateTime.Now;
+                Console.WriteLine("Quelle solution voulez-vous?");
+                Console.WriteLine("Taper 1 - Par solution humaine");
+                Console.WriteLine("Taper 2 - Par solution machine");
+                Console.WriteLine("Taper q - Quitter SudokuSolver");
+                solution_choisie = Console.ReadLine();
 
-                TimeSpan tps_total = end - start;
+                if (solution_choisie == "1") 
+                {
+                    // On définit un objet de type "Sudoku"
+                    var sudokuHumain = new SudokuHumain(grille_finale);
+                    sudokuHumain.afficherSudoku();
 
-                // On affiche le Sudoku Finale
-                Console.WriteLine("Temps d'exécution : " + tps_total.Seconds + "s " + tps_total.Milliseconds + "ms");
-                Console.WriteLine("Sudoku Solution Humain :");
-                sudokuHumain.afficherSudoku();  
-            }
+                    DateTime start = DateTime.Now;
 
-            if (solution_choisie == "2")
-            {
+                    // On démarre la résolution en commencant par 0
+                    sudokuHumain.resolution();
 
-                // On définit un objet de type "Sudoku"
-                var sudokuMachine = new SudokuMachine(input);
-                sudokuMachine.afficherSudoku();
+                    DateTime end = DateTime.Now;
 
-                DateTime start = DateTime.Now;
+                    TimeSpan tps_total = end - start;
+
+                    // On affiche le Sudoku Finale
+                    Console.WriteLine("Temps d'exécution : " + tps_total.Seconds + "s " + tps_total.Milliseconds + "ms");
+                    Console.WriteLine("Sudoku Solution Humain :");
+                    sudokuHumain.afficherSudoku();  
+                }
+
+                if (solution_choisie == "2")
+                {
+
+                    // On définit un objet de type "Sudoku"
+                    var sudokuMachine = new SudokuMachine(grille_finale);
+                    sudokuMachine.afficherSudoku();
+
+                    DateTime start = DateTime.Now;
                 
-                // On démarre la résolution en commencant par 0
-                sudokuMachine.estValide(0);
+                    // On démarre la résolution en commencant par 0
+                    sudokuMachine.estValide(0);
 
-                DateTime end = DateTime.Now;
+                    DateTime end = DateTime.Now;
 
-                TimeSpan tps_total = end - start;
+                    TimeSpan tps_total = end - start;
 
-                // On affiche le Sudoku Finale
-                Console.WriteLine("Temps d'exécution : "+ tps_total.Seconds + "s " + tps_total.Milliseconds +"ms");
-                Console.WriteLine("Sudoku Solution Machine :");
-                sudokuMachine.afficherSudoku();                
-            }
+                    // On affiche le Sudoku Finale
+                    Console.WriteLine("Temps d'exécution : "+ tps_total.Seconds + "s " + tps_total.Milliseconds +"ms");
+                    Console.WriteLine("Sudoku Solution Machine :");
+                    sudokuMachine.afficherSudoku();                
+                }
 
+            } while (solution_choisie == "q" || solution_choisie == "Q");
+            
             // On stoppe le programme
             Console.ReadLine();
         }
--- SudokuHumain.cs
+++ SudokuHumain.cs
@@ -38,135 +38,84 @@ namespace Sudoku
             Console.WriteLine("\n\n");
         }
 
-        /* Méthode Absent sur la ligne */
+        /*************************************************************************************************************/
+        //pour déterminer l'absence d'une valeur sur une ligne
         public bool absentSurLaLigne(int valeur, int ligne)
         {
+            //parcours le long de la ligne
             for (int col = 0; col < 9; col++)
             {
+                //vérification de la présence de la valeur
                 if (Grid[ligne][col] == valeur)
                     return false;
             }
 
+            //retour true si valeur absente de la ligne
             return true;
         }
 
-        /* Méthode Absent sur la colonne */
+        /*************************************************************************************************************/
+        //pour déterminer l'absence d'une valeur sur une colonne
         public bool absentSurLaColonne(int valeur, int col)
         {
+            //parcours le long de la colonne
             for (int ligne = 0; ligne < 9; ligne++)
             {
+                //vérification de la présence de la valeur
                 if (Grid[ligne][col] == valeur)
                     return false;
             }
-
+            //retour true si valeur absente de la colonne
             return true;
         }
 
-        /* Méthode Absent dans la région */
+        /*************************************************************************************************************/
+        //pour déterminer l'absence d'une valeur sur une ligne
         public bool absentSurRegion(int valeur, int ligne, int col)
         {
+            //détermination de la région relative à la case donnée par ligne et col
             int _ligne = ligne - (ligne % 3), _col = col - (col % 3);
+
+            //parcours de la région en ligne
             for (ligne = _ligne; ligne < _ligne + 3; ligne++)
             {
+                //parcours de la région en colonne
                 for (col = _col; col < _col + 3; col++)
                 {
+                    //vérification de présence de la valeur
                     if (Grid[ligne][col] == valeur)
                         return false;
                 }
             }
 
+            //retour vrai si valeur absente de la région
             return true;
         }
 
+        /*************************************************************************************************************/
         //pour compter le nombre de cases remplies initialement dans la grille
         public int compterNbCasesRemplies()
         {
             int nbCasesRemplies = 0;
             int i, j;
 
+            //parcours de la grille
             for (i = 0; i < 9; i++)
             {
                 for (j = 0; j < 9; j++)
                 {
+                    //si une case contient une valeur
                     if (Grid[i][j] != 0)
+                        //incrémentation de nbCasesRemplies
                         nbCasesRemplies++;
                 }
             }
-
+            //renvoi du nb de cases remplies dans la grille
             return nbCasesRemplies;
         }
 
-        public void resolutionSurUneRegion(ref int nbCasesRemplies, int ligne, int col, int valeur)
-        {
-
-            int ligneRetenue = 0, colRetenue = 0, nbCasesCandidates = 0;
-            int i, j;
-
-            for (i = ligne; i < ligne + 3; i++)
-            {
-
-                for (j = col; j < col + 3; j++)
-                {
-
-                    //si la case contient déjà qqch, on l'ignore
-                    if (Grid[i][j] != 0)
-                    {
-                        continue;
-                    }
-
-                    //si la valeur peut potentiellement entrer dans une case
-                    if (absentSurLaColonne(valeur, j) && absentSurLaLigne(valeur, i) && absentSurRegion(valeur, i, j))
-                    {
-                        //on incrémente le nb de cases dans laquelle la valeur peut s'y insérer
-                        nbCasesCandidates++;
-
-                        //on sauvegarde la dernière case où la valeur peut entrer
-                        ligneRetenue = i;
-                        colRetenue = j;
-                    }
-
-                }//fin for(col)
-
-
-            }//fin for(ligne)
-
-            //s'il n'y a qu'une seule valeur possible sur la ligne
-            if (nbCasesCandidates == 1)
-            {
-
-                //on affecte la valeur dans cette case
-                Grid[ligneRetenue][colRetenue] = valeur;
-
-                //incrémentation du nb de cases remplies
-                nbCasesRemplies++;
-
-            }
-
-        }//fin fonction resolutionSurUneRegion()
-
-        public void resolutionSurToutesRegions(ref int nbCasesRemplies)
-        {
-            int valeur, ligne=0, col=0;
-
-            //la boucle s'arrête lorsque toutes les régions sont traitées
-
-            //test de chaque valeur pour chacune des cases vides d'une région
-            for (ligne = 0; ligne < ligne + 3 && ligne<9; ligne += 3)
-            {
-                for (col = 0; col < col + 3 && col<9; col += 3)
-                {
-                    for (valeur = 1; valeur <= 9; valeur++)
-                    {
-                        resolutionSurUneRegion(ref nbCasesRemplies, ligne, col, valeur);
-
-                    }//fin for(valeur)
-
-                }//fin for (col)
-
-            }//fin for (ligne)
-
-        }
-
+        /*************************************************************************************************************/
+        //méthode de résolution sur une ligne
         public void resolutionSurLigne(ref int nbCasesRemplies)
         {
 
@@ -181,7 +130,7 @@ namespace Sudoku
                 //test de chaque valeur pour savoir si ça peut entrer dans une case de la ligne
                 for (valeur = 1; valeur <= 9; valeur++)
                 {
-
+                    //parcours colonne par colonne
                     for (col = 0; col < 9; col++)
                     {
                         //si la case contient déjà qqch, on l'ignore
@@ -201,7 +150,7 @@ namespace Sudoku
 
                     }//fin for(col)
 
-                    //s'il n'y a qu'une seule valeur possible sur la ligne
+                    //puis s'il n'y a qu'une seule valeur possible sur la ligne
                     if (nbCasesCandidates == 1)
                     {
                         //on affecte la valeur dans cette case
@@ -218,8 +167,11 @@ namespace Sudoku
                 }//fin for(valeur)
 
             }//fin for(ligne)
-        }
 
+        }//fin de resolutionSurLigne
+
+        /*************************************************************************************************************/
+        //résolution sur une colonne
         public void resolutionSurColonne(ref int nbCasesRemplies)
         {
 
@@ -234,7 +186,7 @@ namespace Sudoku
                 //test de chaque valeur pour savoir si ça peut entrer dans une case de la ligne
                 for (valeur = 1; valeur <= 9; valeur++)
                 {
-
+                    //parcours colonne par colonne
                     for (ligne = 0; ligne < 9; ligne++)
                     {
                         //si la case contient déjà qqch, on l'ignore
@@ -271,19 +223,96 @@ namespace Sudoku
                 }//fin for(valeur)
 
             }//fin for(col)
-        }
 
+        }//fin de resolutionSurColonne
+
+        /*************************************************************************************************************/
+        //résolution sur une seule et unique région
+        public void resolutionSurUneRegion(ref int nbCasesRemplies, int ligne, int col, int valeur)
+        {
+
+            int ligneRetenue = 0, colRetenue = 0, nbCasesCandidates = 0;
+            int i, j;
+
+            //parcours de la région
+            for (i = ligne; i < ligne + 3; i++)
+            {
+
+                for (j = col; j < col + 3; j++)
+                {
+
+                    //si la case contient déjà qqch, on l'ignore
+                    if (Grid[i][j] != 0)
+                        continue;
+
+                    //si la valeur peut potentiellement entrer dans une case
+                    if (absentSurLaColonne(valeur, j) && absentSurLaLigne(valeur, i) && absentSurRegion(valeur, i, j))
+                    {
+                        //on incrémente le nb de cases dans laquelle la valeur peut s'y insérer
+                        nbCasesCandidates++;
+
+                        //on sauvegarde la dernière case où la valeur peut entrer
+                        ligneRetenue = i;
+                        colRetenue = j;
+                    }
+
+                }//fin for(col)
+
+
+            }//fin for(ligne)
+
+            //s'il n'y a qu'une seule valeur possible sur la région
+            if (nbCasesCandidates == 1)
+            {
+
+                //on affecte la valeur dans cette case
+                Grid[ligneRetenue][colRetenue] = valeur;
+
+                //incrémentation du nb de cases remplies
+                nbCasesRemplies++;
+
+            }
+
+        }//fin fonction resolutionSurUneRegion()
+
+        /*************************************************************************************************************/
+        public void resolutionSurToutesRegions(ref int nbCasesRemplies)
+        {
+            int valeur, ligne = 0, col = 0;
+
+            //double boucle for pour parcourir chaque région
+            for (ligne = 0; ligne < ligne + 3 && ligne < 9; ligne += 3)
+            {
+                for (col = 0; col < col + 3 && col < 9; col += 3)
+                {
+                    //test de toutes les valeurs
+                    for (valeur = 1; valeur <= 9; valeur++)
+                    {
+                        //appel à la fonction auxiliaire qui vérifie sur une région si la valeur courante peut s'y insérer
+                        //incrémentation de nbCasesRemplies si des cases sont remplies
+                        resolutionSurUneRegion(ref nbCasesRemplies, ligne, col, valeur);
+
+                    }//fin for(valeur)
+
+                }//fin for (col)
+
+            }//fin for (ligne)
+
+        }//fin méthode resolutionSurToutesRegions
+
+        /*************************************************************************************************************/
+        //méthode de résolution principale
         public void resolution()
         {
             //position donne la position dans la grille
             int nbCasesRemplies = 0, cptboucle = 0;
+
             //compter le nombre de cases remplies
-            nbCasesRemplies=compterNbCasesRemplies();
-            //Console.WriteLine("nbcasesremplies = " + nbCasesRemplies);
+            nbCasesRemplies = compterNbCasesRemplies();
 
             //tant que toutes les cases ne sont pas remplies
             //et tant que le nb de tours de boucle n'a pas atteint 10 000
-            while (nbCasesRemplies < 81 && cptboucle<10000)
+            while (nbCasesRemplies < 81 && cptboucle < 10000)
             {
                 //incrémentation du cpt de boucle
                 cptboucle++;
